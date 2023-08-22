643a8b555809f78a339efcf5b21fa4b4c2cdc14b
Mahdi Salmani <m10.salmani@gmail.com>
2023-08-22 20:12:00 +0330

    fixed!

--- surfree.py
+++ surfree.py
@@ -541,10 +541,6 @@ class Basis:
         for i in np.r_[:dct.shape[2]:8]:
             for j in np.r_[:dct.shape[3]:8]:
                 im_dct[:, :, i:(i+8),j:(j+8)] = self._f_idct2(dct[:, :, i:(i+8),j:(j+8)])
-                print(self._f_idct2(dct[:, :, i:(i+8),j:(j+8)]))
-                print(im_dct[:, :, i:(i+8),j:(j+8)])
-                break
-            break
         return im_dct
 
 
